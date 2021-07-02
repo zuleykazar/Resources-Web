c003c15c2174899abae1107f0b17838e9115e91e
Zuleyka Zaraza <zuleyka.zaraza@gmail.com>
2021-07-01 23:56:42 -0400

    CREADA VALIDACION PARA OBTENER AL USUARIO ACTUAL
    Enlazadas URLS de navbar con sus views. Configuradas links de navbar a mostrar durante sesion abierta y cerrada. Creada autenticacion de usuario para acceder a links dentro de su sesion. Creado redirect de views con login y register

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import render_template, request, flash, redirect, url_for 
 
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from .models import User
 from .forms import LoginForm, RegisterForm
 from app.consts import *
@@ -38,6 +38,10 @@ def logout():
 
 @page.route('/login', methods = ['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('.tasks'))
+
     form = LoginForm(request.form)
     
     if request.method == 'POST' and form.validate():
@@ -45,6 +49,7 @@ def login():
         if user and user.verify_password(form.password.data):
             login_user(user)
             flash(LOGIN)
+            return redirect(url_for('.tasks'))
         else:
             flash(ERROR_USER_PASSWORD, 'error')
 
@@ -52,12 +57,18 @@ def login():
 
 @page.route('/register', methods = ['GET', 'POST'])   
 def register():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('.tasks'))
+
     form = RegisterForm(request.form)
 
     if request.method == 'POST':
         if form.validate():
             user = User.create_element(form.username.data, form.password.data, form.email.data)
             flash(USER_CREATED)
+            login_user(user)
+            return redirect(url_for('.tasks'))
 
 
     return render_template('auth/register.html', title='Registro', form = form) 
